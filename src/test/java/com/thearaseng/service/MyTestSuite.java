b356684b96f07c04c9ac7b14e132615e84bc7e20
Theara Seng <seng.theara.kh@gmail.com>
2018-07-12 14:01:05 +0700

    create a simple Test Suite

---
+++ src/test/java/com/thearaseng/service/MyTestSuite.java
@@ -0,0 +1,12 @@
+package com.thearaseng.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        HelloWorldTest.class,
+        TrackingServiceTest.class
+})
+public class MyTestSuite {
+}
