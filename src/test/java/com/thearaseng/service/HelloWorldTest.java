7d26f69f32c46ce878c853e2caa6e970c744c753
Theara Seng <seng.theara.kh@gmail.com>
2018-07-12 14:00:50 +0700

    create HelloWorldTest

---
+++ src/test/java/com/thearaseng/service/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.thearaseng.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+public class HelloWorldTest {
+
+    @Test
+    public void shouldSayHelloWorld() {
+        assertEquals("Should say 'Hello World'", "Hello World", "Hello World");
+    }
+
+}
